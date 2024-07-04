a1f3b1b839fb1e1a1b2682a750d7081bc6985d01
FM-Akib <muntasirakib08@gmail.com>
2024-07-04 20:13:04 +0600

    hero updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-[#EFFBE3] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
         <Navbar/>
         <div className="md:px-16">
         {children}
--- src/components/ui/Hero.js
+++ src/components/ui/Hero.js
@@ -5,15 +5,21 @@ import linkedin from '../../assets/linkedin.svg';
 import youtube from '../../assets/youtube.svg';
 import facebook from '../../assets/facebook.svg';
 import github from '../../assets/github.svg';
+import '../../app/globals.css'
+import { FaLaptopCode } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+
+
 
 const Hero = () => {
     return (
         <div className="flex  px-6 md:px-20  items-center justify-center bg-hero md:h-screen overflow-hidden">
-    <div className="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
+    <div className="flex flex-col-reverse  gap-6 md:flex-row items-center max-w-8xl">
 
-        <div className="gap-4  md:mt-20  flex md:flex-col ">
+        <div className="gap-4  md:mt-20  flex md:flex-col items-center ">
                 <div className="icon">
-                    <Link target="_blank" href="https://www.linkedin.com/in/mohammad-fahim-muntasir-akib-a1aa50215/" >
+                    <Link target="_blank" href="https://www.linkedin.com/in/fahim-muntasir-akib/" >
                         <Image height={34} width={35} src={linkedin} alt="linkedIn" />
                     </Link>
                 </div>
@@ -23,7 +29,7 @@ const Hero = () => {
                     </Link>
                 </div>
                 <div className="icon">
-                    <Link target="_blank" href="https://github.com/Motimiya08">
+                    <Link target="_blank" href="https://github.com/FM-Akib">
                         <Image height={35} width={35} src={github} alt="github" />
                     </Link>
                 </div>
@@ -35,16 +41,27 @@ const Hero = () => {
         </div>
 
         <div className="w-full md:w-1/2 ">
-            <h2 className="text-3xl lg:text-5xl text-center md:text-left text-[#2A2626] leading-tight font-medium">
+            <h2 className="caveat-brush-regular text-xl lg:text-5xl text-center md:text-left text-[#393939] leading-tight font-medium">
                 Mohammad Fahim Muntasir Akib.</h2>
-            <p class="text-emerald-500 my-1">Web Developer - Mern Stack Developer</p>
-            <p className="mt-6 md:mt-10 text-md lg:text-md text-center md:text-left text-gray-700  leading-relaxed">
+                <div className="flex  items-center justify-start my-2 bg-gradient-to-tr  ">
+                <div className="w-max flex items-center">
+                    
+                <FaLaptopCode className="mr-2 text-emerald-500" />
+                    <p
+                    className="text-emerald-500 animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-emerald-500  font-semibold">
+                    Web Developer - Mern Stack Developer -
+                    </p>
+                </div>
+                </div>
+          
+            <p className="mt-6 md:mt-10 text-md lg:text-md text-center md:text-left text-gray-600  leading-relaxed">
                Hi, I specialize in MERN Stack web development, proficiently navigating through HTML, CSS, Tailwind, React, JavaScript, Express.js, and MongoDB. Beyond the technicalities, I am adept in C and C++, with a keen eye for optimizing code and enhancing performance. As an undergraduate Teaching Assistant in Object-Oriented Programming C++, I've honed my skills in guiding and mentoring others through complex programming concepts.
 
             </p>
             <div className="mt-10 flex flex-col sm:flex-row justify-center md:justify-start">
-                <button className="w-full sm:w-40 px-4 py-3 rounded font-semibold text-md bg-blue-500 text-white border-2 border-blue-500">Get started</button>
-                <button className="w-full mt-4 sm:mt-0 sm:ml-4 sm:w-40 px-4 py-3 rounded font-semibold text-md bg-white text-blue-500 border-2 border-gray-500">Watch a Demo</button>
+                <button className="w-full flex items-center justify-center sm:w-40 px-4 py-3 rounded font-semibold text-md bg-emerald-500 text-white border-2 border-emerald-500">Contact Me <FaRegArrowAltCircleRight className='ml-2' /></button>
+                <button className="w-full flex items-center justify-center mt-4 sm:mt-0 sm:ml-4 sm:w-40 px-4 py-3 rounded font-semibold text-md bg-white hover:bg-slate-100 text-emerald-500 border-2 border-gray-300">Resume <LuDownload className='ml-2' />
+                </button>
             </div>
         </div>
 
